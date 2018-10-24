caee8cceb0fc981d33f5a9cfbd0b0314cbee0c4b
Marin Peko <mpeko00@fesb.hr>
2018-10-24 16:53:10 +0200

    Add BLE code to arduino project

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 #define LED_BUILTIN 13
+
+SoftwareSerial ble(2, 3); /* RX, TX */
 
 void setup() {
     /* initialize LED digital pin as output */
     pinMode(LED_BUILTIN, OUTPUT);
+
+    /* open serial port */
+    Serial.begin(9600);
+
+    /* begin bluetooth serial port communication */
+    ble.begin(9600);
 }
 
 void loop() {
@@ -19,4 +28,8 @@ void loop() {
 
     /* wait for 1s */
     delay(1000);
+
+    Serial.println("Sending Bluetooth Message...");
+    ble.write("Testing...");
+    delay(500);
 }
